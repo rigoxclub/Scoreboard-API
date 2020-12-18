7c697fa907edc32d9c9b3f07a533ea5362e46774
sebastnchan <vauntex18@gmail.com>
2020-12-18 00:15:41 -0300

    Hmm

--- src/main/java/club/rigox/scoreboard/scoreboardapi/ScoreboardAPI.java
+++ src/main/java/club/rigox/scoreboard/scoreboardapi/ScoreboardAPI.java
@@ -3,15 +3,11 @@ package club.rigox.scoreboard.scoreboardapi;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ScoreboardAPI extends JavaPlugin {
+    public static ScoreboardAPI instance;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
 
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
     }
 }
